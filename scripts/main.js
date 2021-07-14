80fd284b8514780b74ee85d8e46bd33b7b2cad8e
Pepetka <vlad.kuzntsov@mail.ru>
2021-07-14 17:18:36 +0300

    new concept

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,27 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/Firefox-logo.webp') {
-        myImage.setAttribute('src','images/Firefox2.png');
+    if (mySrc === '../images/web.png') {
+        myImage.setAttribute('src','../images/html-css-js.png');
     }
     else {
-        myImage.setAttribute('src','images/Firefox-logo.webp');
+        myImage.setAttribute('src','../images/web.png');
     }
 }
 var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
+var myHeading = document.querySelector('#приветствие');
 
 function setUserName() {
     var myName = prompt('Enter your name.');
     localStorage.setItem('name',myName);
-    myHeading.textContent = 'Привет, ' + myName + '. Это тест страница.';
+    myHeading.textContent = 'Привет, ' + myName + '. Добро пожаловать на страницу.';
 }
 if (!localStorage.getItem('name')) {
     setUserName();
 }
 else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Привет, ' + storedName + '. Это тест страница.';
+    myHeading.textContent = 'Привет, ' + storedName + '. Добро пожаловать на страницу.';
 }
 
 myButton.onclick = function() {
